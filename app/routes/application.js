60968c5a91ab2f52efb454fafee9e85717fac6ad
KlausLee <skyfpt1@gmail.com>
2017-09-05 17:21:34 +0900

    assignment #034

---
+++ app/routes/application.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(){
+
+    function timeFunc(){
+
+      var date = new Date();
+      var hour = date.getHours();
+      var min = date.getMinutes();
+      var sec = date.getSeconds();
+
+      if(hour < 10)
+        hour = "0" + hour;
+      if(min < 10)
+        min = "0" + min;
+      if(sec < 10)
+        sec = "0" + sec;
+
+      var time = hour + ":" + min + ":" + sec;
+
+      //document.open() 으로 stream 초기화
+      document.open();
+      document.write(time);
+    }
+
+    setInterval(timeFunc, 1000);
+  }
+});
